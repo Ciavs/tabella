8911a002a70e2e8d38fbf6c6c0232e6550d41bac
lai2-01 <lai2-01@itismeucci.com>
2023-05-17 08:32:55 +0000

    manca modifica

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Tabella from './components/Tabella'
+
+function App() {
+
+  return (
+    <>
+      <Tabella></Tabella>
+    </>
+  )
+}
+
+export default App
